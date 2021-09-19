ff379308e8e120cc8d2b2b0fd1c870cb135bed42
tanishk15sharma <tanishk1582002@gmail.com>
2021-09-19 19:14:23 +0530

    css-added

--- app.js
+++ app.js
@@ -1,53 +1,41 @@
-var  initialPrice = document.querySelector("#initial-price")
+var initialPrice = document.querySelector("#initial-price")
 var stocksQuantity = document.querySelector("#stocks-quantity")
 var currentPrice = document.querySelector("#current-price")
 var submitBtn = document.querySelector("#submit-btn")
 var outputBox = document.querySelector("#output-box")
 
-submitBtn.addEventListener("click",submitHandler)
+submitBtn.addEventListener("click", checkHandler)
 
-function submitHandler() {
+function checkHandler() {
     var ip = initialPrice.value
     var qty = stocksQuantity.value
     var curr = currentPrice.value
 
-    calculateProfitAndLoss(ip,qty,curr)
+    calculateProfitAndLoss(ip, qty, curr)
 }
 
 
 
-function calculateProfitAndLoss(initial,quantity,current)
+function calculateProfitAndLoss(initial, quantity, current)
 
 {
-    if(initial > current ) {
-        var loss = (initial-current) * quantity 
-        var lossPercentage = (loss/initial) * 100;
+    if (initial > current) {
+        var loss = (initial - current) * quantity
+        var lossPercentage = (loss / initial) * 100;
 
-        showOutput("oops! its a loss ");
+        showOutput(`oops it's loss ${loss} ! The loss percentage is ${lossPercentage}  `);
 
-        
-        
-        
+
+    } else if (current > initial) {
+        var profit = (current - initial) * quantity
+        var profitPercentage = (profit / initial) * 100;
+        showOutput(`wow it's profit ${profit} ! The profit percentage is ${profitPercentage}  `);
+    } else {
+        showOutput("no gain , no change ");
     }
-else if (current > initial ) {
-    var profit = (current -initial ) * quantity
-    var profitPercentage = (profit/initial )*100;
-    showOutput("wow ! its profit  ");
-}
-else {
-    showOutput("no gain , no change ");
-}
 }
 
-function showOutput(message ) {
+function showOutput(message) {
 
     outputBox.innerHTML = message
 }
-
-
-
-
-
-
-
-
